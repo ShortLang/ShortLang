1dfb3db58c1d4eda06ad77280f7aac842ea0904e
Pranjal-Patel <pranjalpatel9838@gmail.com>
2024-01-25 16:45:29 +0530

    moved macro

--- src/vm/stdlib/macros.rs
+++ src/vm/stdlib/macros.rs
@@ -31,3 +31,24 @@ macro_rules! add_fn {
         )*
     }
 }
+
+#[macro_export]
+macro_rules! convert_to_i128 {
+    ($value:expr) => {
+        match $value {
+            Value::Int(i) => i.saturating_cast(),
+            Value::Float(f) => f
+                .to_integer()
+                .unwrap_or_else(|| {
+                    // Only way for the unwrap to fail is if the float is infinity
+                    if f.is_sign_negative() {
+                        Integer::from(i128::MIN)
+                    } else {
+                        Integer::from(i128::MAX)
+                    }
+                })
+                .saturating_cast(),
+            _ => ret!(err: "Expected a number"),
+        }
+    };
+}
